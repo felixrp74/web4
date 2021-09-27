356de645da4060730b4caa0a78261fa690b1babb
felixrp74 <felixrp74@gmail.com>
2021-09-26 23:59:17 -0500

    subir proyecto web4 inventario

---
+++ Controllers/MovimientoController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web4.Models;
+using Web4.Models.ViewModels;
+
+namespace Web4.Controllers
+{
+    public class MovimientoController : Controller
+    {
+        // GET: Movimiento
+        public ActionResult Anadir()
+        {
+            return View();
+        }
+        //POST: Movimiento
+
+        [HttpPost]
+        public ActionResult Anadir(MovimientoViewModel model)
+        {
+            try
+            {
+                using (Prueba4Entities db = new Prueba4Entities())
+                {
+                    Responsable oResponsable = new Responsable();
+                    oResponsable.Nombre = model.Nombre;
+                    oResponsable.Cargo = model.Cargo;
+
+                    db.Responsable.Add(oResponsable);
+                    db.SaveChanges();
+
+                    
+                    Ficha oFicha = new Ficha();
+                    oFicha.Fecha = DateTime.Now;
+                    oFicha.Origen = model.Origen;
+                    oFicha.Destino = model.Destino;
+                    oFicha.TipoMovimiento = model.TipoMovimiento;
+                    oFicha.ResponsableDelMovimiento = model.ResponsableDelMovimiento;
+                    oFicha.Responsable_Clave_R = oResponsable.Clave_R;
+
+                    db.Ficha.Add(oFicha);
+                    db.SaveChanges();
+
+
+                    foreach (var mE in model.Equipos)
+                    {
+                        Bien oBien = new Bien();
+                        oBien.Descripcion = mE.Descripcion;
+                        oBien.Marca = mE.Marca;
+                        db.Bien.Add(oBien);
+
+                        db.SaveChanges();
+
+                        Detalle oDetalle = new Detalle();
+                        oDetalle.Ficha_Clave_F = oFicha.Clave_F;
+                        oDetalle.Bien_Clave_B = oBien.Clave_B;
+
+                        db.Detalle.Add(oDetalle);
+
+                    } 
+
+                    db.SaveChanges();
+                    
+                    ViewBag.Message = "Registro insertado";
+
+                    return View();
+
+                }
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = "No insertado Error";
+                return View();
+            }
+             
+             
+        }
+    }
+}
---
+++ Models/ViewModels/MovimientoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web; 
+
+namespace Web4.Models.ViewModels
+{
+    public class MovimientoViewModel
+    {
+        public string Nombre { set; get; }
+        public string Cargo { set; get; }
+
+        public DateTime Fecha { set; get; }
+        public string Origen { set; get; }
+        public string Destino { set; get; }
+        public string TipoMovimiento { set; get; }
+        public string ResponsableDelMovimiento { set; get; }
+
+        public List<Bienes> Equipos { set; get; }
+        
+    }
+
+    public class Bienes
+    {
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Serie { get; set; }
+        public string Estado { get; set; }
+        public string UsuarioPC { get; set; }
+        public string NombrePC { get; set; }
+        public bool Devuelto { get; set; }
+        
+    }
+}
