c0150dd64c96b89652a66e4941f04c249d19e3a6
pracgsi01 - Ramos Paredes Felix <pracgsi01@electropuno.com.pe>
2021-09-29 22:04:43 -0500

    tabla de responsables sin optimizar al millon de registros

--- Controllers/MovimientoController.cs
+++ Controllers/MovimientoController.cs
@@ -10,6 +10,14 @@ namespace Web4.Controllers
 {
     public class MovimientoController : Controller
     {
+        public string draw = "";
+        public string start = "";
+        public string length = "";
+        public string sortColumn = "";
+        public string sortColumnDir = "";
+        public string searchValue = "";
+        public int pageSize, skip, recordsTotal;
+
         // GET: Movimiento
         public ActionResult Anadir()
         {
@@ -79,10 +87,53 @@ namespace Web4.Controllers
             catch(Exception ex)
             {
                 ViewBag.Message = "No insertado Error";
-                return View();
+                return View(ex);
             }
              
              
         }
+    
+        public ActionResult Vista()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Json()
+        {
+            List<TableResponsableViewModel> lst = new List<TableResponsableViewModel>();
+
+            //logistica datatables
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            var sortColumn = Request.Form.GetValues("columns["+Request.Form.GetValues("order[0][column]").FirstOrDefault()+"][name]" ).FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+            var searchValue = Request.Form.GetValues("search[value]").FirstOrDefault();
+
+            pageSize = length != null ? Convert.ToInt32(length) : 0;
+            skip = start != null ? Convert.ToInt32(start) : 0;
+            recordsTotal = 0;
+
+            //conexion base datos
+            using (Prueba4Entities1 db = new Prueba4Entities1())
+            {
+                lst = (from d in db.Responsable
+                       where d.Nombre.Contains(searchValue)
+                       select new TableResponsableViewModel
+                       {
+                           Clave_R = d.Clave_R,
+                           Nombre = d.Nombre,
+                           Cargo = d.Cargo
+                       }).ToList();
+
+                recordsTotal = lst.Count();
+                lst = lst.Skip(skip).Take(pageSize).ToList();
+
+            }
+
+            return Json(new { draw=draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data=lst });
+        }
+
     }
 }
---
+++ Models/ViewModels/TableResponsableViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web4.Models.ViewModels
+{
+    public class TableResponsableViewModel
+    {
+        public int Clave_R { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+    }
+
+}
